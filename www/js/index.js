3ad2ae38c2f709a4bb77897c30afd4ae60150b08
WANG Guoqin <wangguoqin1001@gmail.com>
2014-10-27 06:06:37 +0800

    demo html

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,35 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+$(document).ready(function(){
+	$("#next").click(function(){
+		if (!$("#edit").val()){
+			alert("请输入内容先!!");
+			return;
+		}else{
+			showSecondPage();
+		}
+	});
+	$("#second").click(function(){
+		showSecondPage();
+	});
+	$("#first").click(function(){
+		showFirstPage();
+	});	
+});	
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function showFirstPage(){
+	$("#panel2").css("display", "none");
+	$("h1").text("编辑");
+	$("#panel1").css("display", "block");
+	$("#first").addClass("pressed");
+	$("#second").removeClass("pressed");
+}
 
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+function showSecondPage(){
+	$("#second").addClass("pressed");
+	$("#first").removeClass("pressed");
+	$("#panel1").css("display", "none");
+	$("#panel2").css("display", "block");
+	$("h1").text("显示");
+	$("#show").val($("#edit").val());
+}	
+		
+		
